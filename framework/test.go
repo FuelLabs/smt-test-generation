9e4fc501586c84cadee418343182f329c42fe671
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-04-04 15:19:07 -0400

    Initial commit

---
+++ framework/test.go
@@ -0,0 +1,36 @@
+package framework
+
+import (
+	"encoding/hex"
+)
+
+type EncodedValue struct {
+	Value    string `json:"value"`
+	Encoding string `json:"encoding"`
+}
+
+type Step struct {
+	Action string        `json:"action"`
+	Key    *EncodedValue `json:"key"`
+	Data   *EncodedValue `json:"data,omitempty"`
+}
+
+type Test struct {
+	Name  string       `json:"name"`
+	Root  EncodedValue `json:"expected_root"`
+	Steps []Step       `json:"steps"`
+}
+
+func HexValue(data []byte) EncodedValue {
+	return EncodedValue{
+		Value:    hex.EncodeToString(data),
+		Encoding: "hex",
+	}
+}
+
+func Utf8Value(data []byte) EncodedValue {
+	return EncodedValue{
+		Value:    string(data),
+		Encoding: "utf-8",
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module smttestgen
+
+go 1.17
+
+require github.com/celestiaorg/smt v0.2.1-0.20211207172250-6e634fe44240 // indirect
